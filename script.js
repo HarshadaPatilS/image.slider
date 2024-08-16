3b22bf4bb6d4d4e72edaaa30bd19e7173afcca4c
Harshada Patil <168923120+HarshadaPatilS@users.noreply.github.com>
2024-08-16 12:20:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// script.js
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+setInterval(nextSlide, 3000); // Change image every 3 seconds
+
+// Optionally, add event listeners for swipe gestures if desired
